7edfe9eef6e94d01df6bc1452ca172a8fc4c4cb0
Andrei Svistunou <andrei_svistunou@epam.com>
2023-03-20 09:12:29 +0100

    add import product functionality

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://13lu3fijvk.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://2kmr71dtvh.execute-api.us-east-1.amazonaws.com",
   bff: "https://13lu3fijvk.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
